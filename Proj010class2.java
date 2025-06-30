19b675e2de0fba4ce8e72c7b2f4b95c91f197a63
daniel <danialrostami999@gmail.com>
2025-06-30 09:27:36 +0330

    triangle

--- Proj010class2.java
+++ Proj010class2.java
@@ -11,19 +11,31 @@ public class Proj010class2 {
         Scanner scanner = new Scanner(System.in);
         System.out.println("wana continue?[y\n]");
         String answer = scanner.nextLine().toLowerCase();
-        if (answer.equals("n"))
-        System.out.println("thank you!!!");
+        if (answer.equals("n")){
+        System.out.println("thank you!");
         break;
+        }
+        else if (answer.equals("y")){
         System.out.println("--------------");
         System.out.println("secend triangle:");
         Triangle T2 = new Triangle(5, 8);
         T2.PrintInfo();
+        }
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.println("wana continue?[y\n]");
+        String answer2 = scanner.nextLine().toLowerCase();
+        if (answer2.equals("n")){
+        System.out.println("thank you!");
+        break;
+        }
+        else if (answer.equals("y")){
         System.out.println("--------------");
         System.out.println("therd triangle:");
         Triangle T3 = new Triangle(2, 6);
-        T2.PrintInfo();
+        T3.PrintInfo();
+        }
         System.out.println("--------------");
         break;
-        }
+    }
     }
 }
